fc50a4a3d35c079e86a9af90eb7d6bf65ea7ec4f
pc3-win11 <uehara_atusi@nifty.com>
2022-08-10 13:21:56 +0900

    20220810 PC3 SSH ADD test

---
+++ 20220809_Github_SSH接続.cpp
@@ -0,0 +1,64 @@
+﻿https://www.granfairs.com/blog/staff/sourcetree-ssh
+
+http://granfairs.com/blog/cto/set-ssh-to-backlog
+
+1.SOURCETREEでSSH鍵（キー）を作成する
+
+	key_comment:rsa-key-20220809   ← rsa-PC3に変更する
+
+Key
+
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCDmHsXcyUB4XepNBm6hePyx8PSN+AGOAHjny5ddj6ZqZZvfjupg7ARxr7+aunZ6bxY7tB2WO9endaFfBns3MIhGBsskEcCwMjr1otVQav6MthjiRLo0N6dklJUknVzhyeDWfUPSZNmKqJgwRf3V5AoapY+ZPmyMH/VMnvVGdwr7RZe8dDRgMxYHEWsDBPDS/6BHUG5SMhcTBmg2xRyyAPJKQJc7w3Cz7wVkm4lnPvGgm3HBtMAdwfiXNjwF8qGnHgnFNtzd9eaVYNGV9O2Dmmus0/P24UJhR62kkvYl3nBlqVDmIvU/0B8fXpxDcVFD/bhIQ3B6zrO5lHyGG6KjGor rsa-PC3
+	
+	
+	C:\Users\uehar\.ssh
+			rsa-PC3				
+			PC3.ppy
+
+2.SOURCETREEにSSH鍵（キー）を登録する
+	SourceTreeのメニューバーから「ツール＞SSHエージェントを起動」をクリック。
+	アイコンをダブルクリック。表示された画面で [ Add key ]をクリック。
+	先ほど保存したプライベートキー（秘密鍵）を選択します。		PC3-urugai27-ppk
+	
+3.BACKLOGにSSH鍵（キー）を登録する	
+	SourceTreeでSSH鍵（キー）を作成し、Githubとつなぐ
+	
+	https://github.com/urugai27/
+
+	title:rsa-PC3
+	Key:
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCDmHsXcyUB4XepNBm6hePyx8PSN+AGOAHjny5ddj6ZqZZvfjupg7ARxr7+aunZ6bxY7tB2WO9endaFfBns3MIhGBsskEcCwMjr1otVQav6MthjiRLo0N6dklJUknVzhyeDWfUPSZNmKqJgwRf3V5AoapY+ZPmyMH/VMnvVGdwr7RZe8dDRgMxYHEWsDBPDS/6BHUG5SMhcTBmg2xRyyAPJKQJc7w3Cz7wVkm4lnPvGgm3HBtMAdwfiXNjwF8qGnHgnFNtzd9eaVYNGV9O2Dmmus0/P24UJhR62kkvYl3nBlqVDmIvU/0B8fXpxDcVFD/bhIQ3B6zrO5lHyGG6KjGor rsa-PC3
+	confirm:パスワードを要求される		パスワード:Tjgo1zjpy
+	
+	
+
+
+Bitbucket Pagesへログイン
+Email:athushiuehara@gmail.com
+username:tanagaa		
+パスワード:Tjgo1zjpy
+
+Bitbucket Pagesへログイン
+https://bitbucket.org/
+
+
+プロジェクト名:urugai
+リポジトリ:tanagaa
+
+C:\Users\test\Documents\tanagaa
+
+2.クーロンを選択-> 
+
+Clone in Sourcetree を選択
+
+3.ssh
+	パスフレーズ:Tjgo1zjpyTjgo1zjpyTjgo1zjpy
+
+
+
+4.
+
+ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAqvy31ushUV72s/jo5+U3qNXEyBNIXPeYHbzF6GpKZJCKY04lgTacn37QUKelAv9QAzHMDupdX9wAi9t6aIqSUp5CEttNrkHVVsthTL80i7nwX9RVVPOWy0IkxAjVizMBBatFZWuwFvCIezgmnQiKGT/SaxaEP/HZcLsDAHBAMd2kXQaFNUsrW30vzRp0mpSZDnRCTLdU+//UsB+HRzTdz+GxabKwGhR11/Oh4bYYTNaD04CSlIyQJ4RmJyDFYKZ0kKuJrqMdALXEIcsUHF1oI822L4EdGrg+zziX6V8mCyGAY3kCM2ugE288ZNyQUPSHxVZqH++ubH3yPhRqJdFayQ== rsa-key-20220210
+
+
+
